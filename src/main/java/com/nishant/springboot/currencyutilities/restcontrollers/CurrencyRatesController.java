3890172ca310224186d0dd08220561bb6306965a
Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>
2020-04-21 08:58:41 -0400

    Added Support for all currencies
    1) New endpoint exposed by backend API to get supported currencies.
    
    Signed-off-by: Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>

--- src/main/java/com/nishant/springboot/currencyutilities/restcontrollers/CurrencyRatesController.java
+++ src/main/java/com/nishant/springboot/currencyutilities/restcontrollers/CurrencyRatesController.java
@@ -1,11 +1,16 @@
 package com.nishant.springboot.currencyutilities.restcontrollers;
 
+import com.nishant.springboot.currencyutilities.model.Currency;
 import com.nishant.springboot.currencyutilities.model.Rates;
 import com.nishant.springboot.currencyutilities.restcontrollers.feignclients.CurrencyClientInterface;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 @RestController
@@ -22,4 +27,19 @@ public class CurrencyRatesController {
     public Rates getLatestRates() {
         return currencyFeignClient.getLatestRates(System.getenv("API_KEY"));
     }
+
+    @GetMapping(value = "/supportedCurrencies", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Set<Currency> getSupportedCurrencies() {
+        Rates rates = currencyFeignClient.getLatestRates(System.getenv("API_KEY"));
+        Set<Currency> supportedCurrencies = new TreeSet<>();
+        if(rates !=null && rates.getRates()!=null && !rates.getRates().isEmpty()){
+            Map<String, Double> currencyRates = rates.getRates();
+            currencyRates.forEach((currencyName,currencyRate)->{
+                Currency currency = new Currency(currencyName,currencyName);
+                supportedCurrencies.add(currency);
+            });
+        }
+        return supportedCurrencies;
+    }
+
 }
