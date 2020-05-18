141e5a7ea2525d16f3f7f7db7e44bb5bf43064c5
Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>
2020-05-18 08:39:18 -0400

    Added Support for caching the HTTP Requests
    Signed-off-by: Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>

--- src/main/java/com/nishant/springboot/currencyutilities/configuration/WebConfig.java
+++ src/main/java/com/nishant/springboot/currencyutilities/configuration/WebConfig.java
@@ -1,8 +1,11 @@
 package com.nishant.springboot.currencyutilities.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Value(value = "${currency.api.allowed-origins}")
     String allowedOrigins;
 
+    @Autowired
+    HandlerInterceptor HttpHandlerInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
@@ -18,4 +24,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(HttpHandlerInterceptor);
+    }
 }
